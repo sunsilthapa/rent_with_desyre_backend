852ea0199988e89d865e1c2ad01cbd5bdd6d62bb
Sahanshil Thapa <94502748+sunsilthapa@users.noreply.github.com>
2024-06-22 10:52:06 +0545

    cart routes

---
+++ routes/cartRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const cartController = require('../controllers/cartController');
+const authMiddleware = require('../middleware/authGuard');
+
+router.post('/', authMiddleware, cartController.addToCart);
+router.get('/', authMiddleware, cartController.getUserCart);
+router.delete('/:clothId', authMiddleware, cartController.removeFromCart);
+router.delete('/', authMiddleware, cartController.clearCart);
+
+module.exports = router;
