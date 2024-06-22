1c5b9301fe3b45b5639b2d1e5fbdcbe6df8e7432
Sahanshil Thapa <94502748+sunsilthapa@users.noreply.github.com>
2024-06-22 10:51:33 +0545

    shipping model

---
+++ model/shippingModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const shippingDetailsSchema = new Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  fullName: { type: String, required: true },
+  email: { type: String, required: true },
+  phoneNo: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('ShippingDetails', shippingDetailsSchema);
